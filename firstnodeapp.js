b23f457b2a73f528c7181aea607150a019c1a102
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-01-24 01:57:14 +1100

    Update firstnodeapp.js

--- firstnodeapp.js
+++ firstnodeapp.js
@@ -2,3 +2,6 @@ const fs = require('fs');
 //this will write the file to the hard drive
 fs.writeFileSync('hello.txt','Hello from Node.js');
 //this will create the file in the current directory
+to run this call in terminal
+
+node first-app.js
