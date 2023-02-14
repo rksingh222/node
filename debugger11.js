b9eb56e53d30c417b059d3c7f38eac843af10bf0
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-02-15 00:22:07 +1100

    Create debugger11.js

---
+++ debugger11.js
@@ -0,0 +1,11 @@
+nodejs debugger
+great integration in visual studio
+select app.js file
+go to visual studio=> start debugger
+select enverinoment node.js
+you will see the extra bar at the top which allows to control the debugger
+in debug console => debugger attached
+set a breakpoint before the written line number
+you can also go to view => debug
+you get to see key variable at the left 
+you can define WATCH also by clicking on +
