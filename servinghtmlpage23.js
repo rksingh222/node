bfb6073ac01781b529aefbd4f9144953565bde23
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-02-27 23:11:35 +1100

    Create servinghtmlpage23.js

---
+++ servinghtmlpage23.js
@@ -0,0 +1,85 @@
+//app.js
+
+const http = require('http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const adminRoutes = require('./routes/admin'):
+const shopRoutes = require('./routes/shop');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res, next) => {
+  res.status(404).send('<h1>Page Not Found </h1>');
+});
+app.listen(3000);
+
+//routes/admin.js
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+});
+
+//for handling post request we can use app.post or app.get or app.patch or app.post or app.delete or app.put
+router.post('/product',(req,res,next)=>{
+  console.log(req.body); // the output comes undefined we need to parse it which is not happening so to parse we need to register parser
+  res.redirect('/');
+});
+
+module.exports = Router;
+
+//routes/shop.js
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+//get post will actually to do exact match  instead if we use router.use() it will work differently
+router.get('/',(req,res,next)=>{
+  res.sendFile(path.join(__dirname,'../','views','shop.html'): //this will work both for linux path as well as windows
+               //dirname will point to current folder 
+});
+ 
+module.exports = Router;
+
+//create folder views/add-product.html
+//create folder views/shop.html
+
+//add-product.html
+
+<body>
+  <header>
+  <nav>
+  <ul>
+  <li><a href="/">shop</a></li>
+  <li><a href="/add-product">Add Product</a></li>
+  </ul>
+  </nav>
+  </header>
+  <main>
+    <form action="/add-product" method="POST">
+      <input type="text" name="title">
+      <button type="submit">Add Product</button>
+    </form>
+   </main>
+</body>
+
+//shop.html
+
+<body>
+  <header>
+  <nav>
+  <ul>
+  <li><a href="/">shop</a></li>
+  <li><a href="/add-product">Add Product</a></li>
+  </ul>
+  </nav>
+  </header>
+  <main>
+    <h1>My Products</h1>
+    <p>List of all the products...</p>
+   </main>
+</body>
