895749a74e68f7d39e6499501af6d3966e9f3ae2
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-03-17 20:48:42 +1100

    Create registerroutes45.js

---
+++ registerroutes45.js
@@ -0,0 +1,257 @@
+npm install --save express-handlebars@3.0
+
+    EJS.  <p> <% name=%></p>
+    Pug(Jade) p #{name}
+    Handlebars  <p>{{ name }}</p>
+
+    Installing and implementing pug
+    npm install --save ejs pug express-handlebars 
+
+
+    //app.js
+
+    const http = require('http');
+    const bodyParser = require('body-parser');
+    const express = require('express');
+
+    const app = express();
+
+    const errorController = require('./controllers/error');
+
+    //set global configuration values
+    app.set('view engine','ejs');
+    //because its in views folder if its in someother folder then give that name like example templates
+    app.set('views','views');
+    app.use(errorController.get404);
+
+    const adminRoutes = require('./routes/admin'):
+    const shopRoutes = require('./routes/shop');
+    app.use(bodyParser.urlencoded({extended: false}));
+
+    //to use public folders or give read access to 
+    app.use(express.static(path.join(__dirname,'public'));
+
+    app.use('/admin',adminRoutes);
+    app.use(shopRoutes);
+    app.use((req,res, next) => {
+      //res.status(404).send('<h1>Page Not Found </h1>');
+      //this will look only for pug file
+      res.status(404).render('404',{pageTitle:'page Not found', path:'/404'});
+    });
+    app.listen(3000);
+
+
+   //controller/admin.js
+    const Product = require('../models/product');
+    exports.getAddProduct = (req,res,next)=>{
+     //res.sendFile(path.join(rootDir,'views','add-product.html'));
+     //for pug type of file
+     res.render('add-product', {pageTitle: 'Add Product',path:'/admin/add-product',formsCSS:true, productCSS:true, activeAddProduct:true});
+   };
+
+   exports.postAddProduct = (req,res,next)=>{
+      console.log(req.body); // the output comes undefined we need to parse it which is not happening so to parse we need to register parser
+      const product = new Product(req.body.title);
+      product.save();
+      res.redirect('/');
+    }
+   
+    exports.getProducts = (req,res,next)=>{
+       Product.fetchAll((products) =>{
+        res.render('admin/products',{prods:products,docTitle:'All Products', path:'/admin/products'
+                        });
+      });
+    };
+
+   //controller/shop.js
+   const Product = require('../models/product');
+  
+   exports.getProducts = (req,res,next)=>{
+      Product.fetchAll((products) =>{
+        res.render('shop',{prods:products,docTitle:'All Products', path:'/products'
+                        });
+      });
+    };
+
+    exports.getIndex = (req, res, next)=>{
+        Product.fetchAll((products) =>{
+        res.render('shop/index',{prods:products,docTitle:'Shop', path:'/'
+                        });
+      });
+    };
+    exports.getCart = (req, res, next)=>{
+      res.render('shop/cart', {
+        'shop/cart', {
+        path: '/cart',
+        docTitle: 'Your Cart'
+      });
+    };
+
+    exports.getCheckout =(req,res,next)=>{
+      res.render('shop/checkout',{
+        path: '/checkout',
+        pageTitle: 'checkout'
+      });
+    };
+
+
+    //controller/errors.js
+    export.get404 = (req, res, next) =>{
+      res.status(404).render('404',{pageTitle: 'Page Not Found'});
+    }
+
+    models/prodduct.js
+
+    const fs = require('fs');
+    const path = require('path');
+    const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+    const getProductsFromFile = (cb)=>{
+        fs.readFile(p,(err,fileContent)=>{
+          if(err){
+            cb([]);
+          }else{
+             cb(JSON.parse(fileContent));
+          }
+        });
+    }
+
+    module.exports = Class Product {
+      constructor(title){
+        this.title = title
+      }
+      save(){
+        //create data folder
+          getProductsFromFile(products =>{
+            products.push(this);
+             fs.writeFile(p,JSON.stringify(products),(err)=>{
+            console.log(err);
+             });
+          });
+      }
+      //retrieve all values is a utility function
+      //call this method directly on the class itself
+      //it gives an error
+      //this function does not return anything
+      //it returns undefined
+      //cb is callback
+      static fetchAll(cb){
+        getProductsFromFile(cb);
+      }
+    }
+
+
+    //routes/admin.js
+    const path = require('path');
+    const express = require('express');
+    const router = express.Router();
+    const adminController = require('../controller/admin'):
+
+    router.get('/add-product',adminController.getAddProduct);
+
+    router.get('/products', adminController.getProducts);    
+
+    //for handling post request we can use app.post or app.get or app.patch or app.post or app.delete or app.put
+    router.post('/product',productsController.postAddProduct);
+
+    //module.exports = Router;
+    module.exports = router;
+
+
+    //routes/shop.js
+    const path = require('path');
+    const express = require('express');
+    const router = express.Router();
+    const shopController = require('../controllers/shop');
+
+    //get post will actually to do exact match  instead if we use router.use() it will work differently
+    router.get('/',shopController.getIndex);
+    router.get('/products', shopController.getProducts);
+    router.get('/cart', shopController.getCart);
+    router.get('/checkout', shopController.getCheckout);
+
+    module.exports = Router;
+
+
+
+    views//includes/head.ejs
+
+    <head>
+    <meta>
+    <title></title>
+    </head>
+
+
+    views//include/end.ejs
+
+    </body>
+    </htmL>
+
+    views//includes/navigation.ejs
+    <header class="main-header">
+     <nav class="main_header_nav">
+     <ul class="main_header__item-list">
+     <li class="main-header__item">
+     <a class="<%= path === '/'? 'active':'' %>" href="/">Shop</a>
+     </li>
+     <li class="main-header__item">
+     <a class="<%= path === '/products'? 'active':'' %>" href="/products">products</a>
+     </li>
+     <li class="main-header__item">
+     <a class="<%= path === '/cart'? 'active':'' %>" href="/cart">Cart</a>
+     </li>
+     <li class="main-header__item">
+     <a class="<%= path === '/admin/add-product'? 'active':'' %>" href="/admin/add-prouct">Add Product</a>
+     </li>    
+     <li class="main-header__item">
+     <a class="<%= path === '/admin/products'? 'active':'' %>" href="/admin/products">Admin Products</a>
+     </li>  
+     </ul>
+     </nav>
+     </header>
+
+    //views/admin/products.ejs
+    <%- include('../includes/head.ejs') %>
+    </head>
+    <body>
+    <%- include('../includes/navigation.ejs') %>
+    <%- include('../includes/end.ejs') %>
+    
+
+    //views/shop/cart.ejs
+    <%- include('../includes/head.ejs') %>
+    </head>
+    <body>
+    <%- include('../includes/navigation.ejs') %>
+    <%- include('../includes/end.ejs') %>
+
+    //views/404.ejs
+    <%- include('includes/head.ejs') %>
+    </head>
+    <body>
+      <%- include('includes/naviation.ejs') %>
+      <h1> page Not Found </h1>
+    <%- include('includes/end.ejs') %>
+    //views/add-product.ejs
+    <%- include('includes/head.ejs') %>
+       <link>
+       </link>
+    </head>
+    <body>
+        <%- include('includes/naviation.ejs') %>
+    <form>
+        </form>
+    <%- include('includes/end.ejs') %>
+        
+    //views/shop.ejs
+    <%- include('includes/head.ejs') %>
+    </head>
+     <%- include('includes/naviation.ejs') %>
+    <% if(prods.length > 0) { %>
+      <div>
+      <% for(let product of prods) { %>
+        <%= product.title %>
+      <% } %>
+    <% } else { %>
+    <% } %>
+      <%- include('includes/end.ejs') %>
