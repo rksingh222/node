8c58d1cd1213c5d026dd00a22a4d85a01bcc0d35
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-03-27 22:31:55 +1100

    Create setupmysql58.js

---
+++ setupmysql58.js
@@ -0,0 +1,34 @@
+go here
+https://www.mysql.com
+click on download
+go down and look for MySql Community(GPL) downloads click on it
+select MySqlCommunity Server download the required for ARM type for processor use ARM(my laptop supports ARM version and my PC supports x86 version)
+you dont have to login , no thanks just start my download
+keep all default and agree
+also use Legacy Password Encryption is supported by node and strong is not supported by node js
+give password
+
+now 
+Mysql WorkBench download it
+install it as asked
+once completed
+MYSql Connections 
+it will have a box in that it will have local instance as 3306 and root and localhost:3306
+enter password opens sql server
+right click below schemas and choose "create schema" to enter this editor
+go to schema select schemas it will show schema editor
+schema name is equal database name node-complete
+at the bottom right apply
+again a box say apply
+
+select tables
+name products
+id pk nn un ai
+title varchar(255) nn
+price double nn
+description TEXT
+imageUrl VARCHAR(255)
+
+apply apply close
+clicking on products in the right most icon will show you the entry
+you can fill up the information keep id empty and apply it will auto generate
