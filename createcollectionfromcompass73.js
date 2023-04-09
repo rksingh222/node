ffbcdaa703315951ee769c6ded8b2be7b7b8bd0b
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-04-09 19:44:28 +1000

    Create createcollectionfromcompass73.js

---
+++ createcollectionfromcompass73.js
@@ -0,0 +1,16 @@
+open compass as mentioned in the previous document
+go to shop database
+select create collection
+enter users
+create collection click
+after that click on users
+in documents tab and add data
+insert document
+like this
+{
+  "_id": {
+    "$oid": "64327fe9d43dea05ec0ed2f2"
+  },
+  "name":"rahul",
+  "email":"rahul@test.com"  
+}
