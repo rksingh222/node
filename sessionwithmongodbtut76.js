e6319f51946badad93a0a75137e6b60d9b214532
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-04-14 16:37:17 +1000

    Update sessionwithmongodbtut76.js

--- sessionwithmongodbtut76.js
+++ sessionwithmongodbtut76.js
@@ -1,3 +1,9 @@
 https://expressjs.com/en/resources/middleware/session.html
 
 use connect-mongodb-session
+
+to install
+
+npm install --save connect-mongodb-session
+
+use database store for session not memory store
