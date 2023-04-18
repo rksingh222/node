46b1d280aa52f339698cdefab1874c9bd9a086fd
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-04-19 09:52:53 +1000

    Create expressvalidator82.js

---
+++ expressvalidator82.js
@@ -0,0 +1,6 @@
+search for express validator if you want to understand the doc
+install it 
+npm install --save express-validator
+const { check } = require('express-validator/check');
+
+this helps in server side validation
