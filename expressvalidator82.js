396b38f2a912d67760efd1380d88cfeaddcf7118
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-04-19 10:13:15 +1000

    Update expressvalidator82.js

--- expressvalidator82.js
+++ expressvalidator82.js
@@ -2,5 +2,8 @@ search for express validator if you want to understand the doc
 install it 
 npm install --save express-validator
 const { check } = require('express-validator/check');
+check('email').isEmail()
+
+const { validationResult } = require('express-validator/check');
 
 this helps in server side validation
