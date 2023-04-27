daa76d549307180bebaf28688e28a3df2e3fa4c3
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-04-27 21:18:48 +1000

    Create jsonfileformat89.js

---
+++ jsonfileformat89.js
@@ -0,0 +1,14 @@
+{
+    "name": "Your Name",
+    "age": 29,
+    "courses": [
+        "angular-the-complete-guide",
+        "react-the-complete-guide"
+    ],
+    "profile": {
+        "joined": "2017-05-21",
+        "courses": 2
+    },
+    "averageRating": 4.8,
+    "active": true
+}
