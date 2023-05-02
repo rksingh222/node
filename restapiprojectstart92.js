ebf8f15a1f98f099bd81fa97ea600498c00c1c06
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-02 11:35:21 +1000

    Update restapiprojectstart92.js

--- restapiprojectstart92.js
+++ restapiprojectstart92.js
@@ -8,3 +8,8 @@ npm install --save-dev nodemon
 replace "start": "nodemon app.js"
 npm install --save body-parser //to parse incoming request like req.body.title when you install the body-parser you will be able to get this content
 
+go to postman
+select POST request
+select Body
+select raw /JSON
+http://localhost:8080/feed/post
