5cd2e9ab6f32997186ea233f5b2c8e3fbc712465
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-02 11:07:00 +1000

    Update restapiprojectstart92.js

--- restapiprojectstart92.js
+++ restapiprojectstart92.js
@@ -6,4 +6,5 @@ add "start": "node app.js" in package.json
 npm install --save express
 npm install --save-dev nodemon
 replace "start": "nodemon app.js"
-npm install --save body-parser //to parse incoming request
+npm install --save body-parser //to parse incoming request like req.body.title when you install the body-parser you will be able to get this content
+
