ae5751d725a0044075ba7864906d321080239da8
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-02 11:03:12 +1000

    Create restapiprojectstart92.js

---
+++ restapiprojectstart92.js
@@ -0,0 +1,9 @@
+create a folder restnodeapi
+create app.js
+to create package.json 
+npm init on terminal
+add "start": "node app.js" in package.json
+npm install --save express
+npm install --save-dev nodemon
+replace "start": "nodemon app.js"
+npm install --save body-parser //to parse incoming request
