eb3c3abf1e5b0ab4ecd2d11a951c6f92dca9eae0
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-26 19:34:24 +1000

    Create rest95.js

---
+++ rest95.js
@@ -0,0 +1,19 @@
+Express Validator
+
+npm install --save express-validator
+
+for acessing mongo db and mongoose model
+
+npm install --save mongoose
+
+for uploading images
+
+npm install --save multer
+
+to decrypt data
+
+npm install --save bcryptjs
+
+to create token
+
+npm install --save jsonwebtoken
